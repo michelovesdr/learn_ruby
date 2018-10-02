3d96d7aeff02176c2b1dc1b59020cbe838f15441
Michelle Bocanegra <michelovesdr@gmail.com>
2018-10-02 23:27:46 +0000

    initial commit

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,20 @@
-#write your code here
+def hello(name)
+    if name != ""
+        puts "Hello #{name}!"
+    elsif name == "Michelle"
+        puts "Hello Michelle"
+    elsif name == "Roman"
+        puts "Hello Roman!"
+    else
+        puts "Hello World!"
+    end
+end
+
+def main
+    print "what is your name? "
+    name = gets.chomp
+    hello(name)
+end
+
+
+main if __FILE__ ==$PROGRAM_NAME
