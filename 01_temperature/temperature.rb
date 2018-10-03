d7161f3acbe4121a7721edf515c7905f7a86886d
Michelle Bocanegra <michelovesdr@gmail.com>
2018-10-03 00:18:18 +0000

    added to temperature.rb

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,18 @@
 #write your code here
+# def ftoc (temp)
+#    # T(°C) = (T(°F) - 32) x 5/9
+#    temp = (temp -32) * (5.0/9)
+#    temp.round(2)
+#end
+
+# def ctof (temp)
+#     # T(°F) = (T(°C) + 32) 8 1.8
+#     temp = (temp * 1.8) + 32
+#end
+
+def ftoc(num)
+    num = (num - 32) * (5.0/9)
+end
+def ctof(num)
+    num = (num * 1.8) + 32
+end
