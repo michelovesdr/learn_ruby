07db4e4bdf6bf3631ed6ad4ff792284940b00904
Michelle Bocanegra <michelovesdr@gmail.com>
2018-10-04 23:54:55 +0000

    updated calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
-#write your code here
+def calculator
+  def add(int, int2)
+    puts (0 + 0)
+  end
+  
+  def subtract(num1, num2)
+    puts (num1 - num2)
+  end
+  
+  def sum(array)
+    if(array.empty? == true)
+      puts 0
+    end
+    
+    i = 0
+    sum = 0
+    while i < array.length
+    sum += array[i]
+    i += 1
+    end
+  
+  puts sum
+  end
+
+  i = 0
+  sum = 0
+  while i < array.length
+    sum += array[i]
+    i += 1
+  end
+
+  return sum
+end
